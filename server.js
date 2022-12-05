ebf47c92cc4dcad27723c4bfe5613440d9c57b0e
Sean Buchas <seanbuchas@gmail.com>
2022-12-05 16:31:16 -0500

    inital commit, start some setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const methodOverride = requrie("method-override");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const mainController = require('./controllers/mainController');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(methodOverride('_method'));
+app.use('/main', mainController);
+
+app.get("/", (req, res) => {
+    res.send("Dont Swear Here");
+})
+
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
