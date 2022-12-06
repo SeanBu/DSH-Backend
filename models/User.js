93e1ca7d874ff0ed3b6974193e631e2f99f729ef
Ian <imcbee@terpmail.umd.edu>
2022-12-06 14:03:19 -0500

    updates to User Model

--- models/User.js
+++ models/User.js
@@ -1,24 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username: { type: String, unique: [true, 'Username already in use.'], required: [true, 'Username is requried.'] },
-    password: { type: String, required: [true, 'Password required.'] },
-    firstName: { type: String, required: [true, 'Please enter a first name.'] },
-    lastName: { type: String, required: [true, 'Please enter a last name.'] },
-    email: { type: String, unique: [true, 'Email already in use.'], required: [true, 'Please enter a username.'] },
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: [true, "Username already in use."],
+      required: [true, "Username is required."],
+    },
+    password: { type: String, required: [true, "Password required."] },
+    firstName: { type: String, required: [true, "Please enter a first name."] },
+    lastName: { type: String, required: [true, "Please enter a last name."] },
+    email: {
+      type: String,
+      unique: [true, "Email already in use."],
+      required: [true, "Please enter a username."],
+    },
     skills: [{ type: String }],
-    available: {type: Boolean, default: true },
-    rating: {type: Number },
-    tutorReviews: [{type: mongoose.Types.ObjectId, ref: 'Reviews' }],
-    courses: [{type: mongoose.Types.ObjectId, ref: 'Course'}]
-}, {
-    timestamps: true, toJSON: {
-        transform: (_doc, ret) => {
-            delete ret.password;
-            return ret;
-        }
-    }, id: false
-});
+    available: { type: Boolean, default: true },
+    rating: { type: Number },
+    studentCount: { type: Number, default: 0 },
+    acceptStudents: { type: Boolean, default: true },
+    accountBio: { type: String },
+    location: { type: String },
+    experience: { type: String },
+    tutorReviews: [{ type: mongoose.Types.ObjectId, ref: "Reviews" }],
+    courses: [{ type: mongoose.Types.ObjectId, ref: "Course" }],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (_doc, ret) => {
+        delete ret.password;
+        return ret;
+      },
+    },
+    id: false,
+  }
+);
 
 const User = mongoose.model("User", UserSchema);
 
