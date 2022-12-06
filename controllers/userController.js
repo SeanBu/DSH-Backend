ab4f778ec6e8fc724c9793f9fecf4de4b83c564a
Ian <imcbee@terpmail.umd.edu>
2022-12-06 14:58:39 -0500

    update userController and added user profile route

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,14 +12,27 @@ router.get("/", (req, res) => {
   res.send("Dont swear here main controller");
 });
 
-//! ---------------------User Show Route-----------------------
+//! ---------------------User Profile Show Route-----------------------
+router.get("/profile", async (req, res, next) => {
+  try {
+    const showUserProfile = await User.findById(req.params.id);
+    res.status(201).send(showUserProfile);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error });
+  }
+});
+
+//! ---------------------User Review Show Route-----------------------
+//TODO - need to figure out if we are getting the user that is logged in
+
 router.get("/:id", async (req, res, next) => {
   try {
-    const showUser = await User.findById(
+    const showUserReview = await User.findById(
       req.params.id,
       "tutorReviews"
     ).populate("tutorReviews");
-    res.status(201).send(showUser);
+    res.status(201).send(showUserReview);
   } catch (error) {
     console.log(error);
     res.status(400).json({ error: error });
